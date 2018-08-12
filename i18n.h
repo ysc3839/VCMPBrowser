dd9bb9e3285e543c23960f4074a40e5099c435a5
Richard Yu <yurichard3839@gmail.com>
2018-08-12 10:32:42 +0800

    Add an option to disable i18n.

--- i18n.h
+++ i18n.h
@@ -2,8 +2,9 @@
 
 const LANGID languages[] = {
 	LANG_NEUTRAL,
-	MAKELANGID(LANG_ARABIC, SUBLANG_DEFAULT),
 	MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+#ifndef NO_I18N
+	MAKELANGID(LANG_ARABIC, SUBLANG_DEFAULT),
 	MAKELANGID(LANG_SPANISH, SUBLANG_SPANISH_MODERN),
 	MAKELANGID(LANG_DUTCH, SUBLANG_DEFAULT),
 	MAKELANGID(LANG_POLISH, SUBLANG_DEFAULT),
@@ -11,12 +12,14 @@ const LANGID languages[] = {
 	MAKELANGID(LANG_TURKISH, SUBLANG_DEFAULT),
 	MAKELANGID(LANG_CHINESE, SUBLANG_CHINESE_SIMPLIFIED),
 	MAKELANGID(LANG_BENGALI, SUBLANG_BENGALI_BANGLADESH)
+#endif // NO_I18N
 };
 
 const wchar_t *languageNames[] = {
 	L"Default",
-	L"Arabic",
 	L"English",
+#ifndef NO_I18N
+	L"Arabic",
 	L"Spanish",
 	L"Dutch",
 	L"Polish",
@@ -24,9 +27,14 @@ const wchar_t *languageNames[] = {
 	L"Turkish",
 	L"Chinese (Simplified)",
 	L"Bengali"
+#endif // NO_I18N
 };
 
+#ifndef NO_I18N
 inline wchar_t* LoadStr(wchar_t* origString, UINT ID) { wchar_t* str; return (LoadString(g_hInst, ID, (LPWSTR)&str, 0) ? str : origString); }
+#else // NO_I18N
+#define LoadStr(origString, ID) origString
+#endif // NO_I18N
 
 bool ConvertCharset(const char *from, std::wstring &to)
 {
